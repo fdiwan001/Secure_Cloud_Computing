a112a69a4e8c31db40ec9c28a11c9a6bd859772a
sujoyDU <sujoydebnath.92@gmail.com>
2021-10-28 14:31:20 -0400

    set up quill

--- src/Component/Document/Doc.js
+++ src/Component/Document/Doc.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import TextEditor from './TextEditor'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 
 export default function Doc() {
-    return <TextEditor/> 
+    useEffect(()=> {
+        new Quill('#container', {theme : "snow"})
+    }, [])
+
+    return <div id="container"></div>
 }
