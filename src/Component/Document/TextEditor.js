25317d32b514e5ef8855c9f8708afa650f2aaa56
sujoyDU <sujoydebnath.92@gmail.com>
2021-10-28 14:48:00 -0400

    fixed multiple toolbar issue

--- src/Component/Document/TextEditor.js
+++ src/Component/Document/TextEditor.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React,{useCallback} from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
 
 export default function TextEditor() {
+
+    const wrapperRef = useCallback((wrapper) => {
+        if(wrapper == null) return
+
+        wrapper.innerHTML = ""
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+
+        new Quill( editor, {theme : "snow"})
+        //asddsfsd
+        //sdfsgfdgd
+
+        
+
+    },[])
+
     return (
-        <div>
-            Text Editor
-        </div>
+        <div ID ="container" ref = {wrapperRef}></div>
     )
 }
 
