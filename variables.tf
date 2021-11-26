1d4ec99c6569b9bcc94f0bf1ab6dbfdbd13bdae1
Farhanmadar Diwan <fdiwan3091@gmail.com>
2021-11-26 02:09:49 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = cloud-development-e159d
+}
+
+variable "name" {
+  description = "name prefix for resources"
+  type        = secure-cloudrun-app
+}
+
+variable "image" {
+  description = "container image to deploy"
+  default     = "gcr.io/cloud-development-e159d/github.com/fdiwan001/secure_cloud_computing:latest"
+}
