3db37c7a961d4f390bc9b28f476aa922a8cce5ef
Farhanmadar Diwan <fdiwan3091@gmail.com>
2021-11-29 02:12:15 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,5 +10,5 @@ variable "name" {
 
 variable "image" {
   description = "container image to deploy"
-  default     = "gcr.io/cloud-development-e159d/myapp:latest"
+  default     = "gcr.io/cloud-development-e159d/secure_app:latest"
 }
