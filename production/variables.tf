76227e0477de7378493e73a4bf77622ea5c7098b
fdiwan001 <fdiwan3091@gmail.com>
2021-12-03 03:02:36 -0500

    update v3

--- production/variables.tf
+++ production/variables.tf
@@ -12,7 +12,3 @@ variable "image" {
   description = "container image to deploy"
   default     = "gcr.io/cloud-production-5be1a/secure_app:latest"
 }
-variable "domain" {
-  description = "Zone domain."
-  default     = "securecloudprod.com"
-}
